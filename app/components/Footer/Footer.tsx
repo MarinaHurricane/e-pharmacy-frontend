4c6a124fd6c940e10f22f34ae11f805d93b2eef7
MarinaHurricane <maryishch88@gmail.com>
2026-09-05 20:17:44 +0100

    responsive footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,68 @@
+import Container from '../Container/Container';
+import { Icon } from '../Icon/Icon';
 import css from './Footer.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { NavBar } from '../NavBar/NavBar';
 
 export default function Footer() {
-    return (
-        <div className={css.footer}>Footer</div>
-    )
+  return (
+    <footer className={css.footer}>
+      <Container>
+        <div className={css.footerContainer}>
+          <div className={css.footerSection}>
+            <Link href={'/'}>
+              <Image
+                src="/images/logo_footer.svg"
+                alt="Pharmacy logo"
+                width={135}
+                height={32}
+                className={css.footerLogo}
+              />
+            </Link>
+            <p className={css.footerParagraph}>
+              Get the medicine to help you feel better, get back to your active
+              life, and enjoy every moment.
+            </p>
+          </div>
 
+          <div className={css.footerSectionLinks}>
+            <NavBar variant="footer" />
+
+            <ul className={css.socialsList}>
+              <li className={css.social}>
+                <Link href={'https://www.facebook.com/'}>
+                  <Icon name="icon-facebook" className={css.link} />
+                </Link>
+              </li>
+              <li className={css.social}>
+                <Link href={'https://www.instagram.com/'}>
+                  <Icon name="icon-instagram" className={css.link} />
+                </Link>
+              </li>
+              <li className={css.social}>
+                <Link href={'https://www.youtube.com/'}>
+                  <Icon name="icon-youtube" className={css.link} />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <hr className={css.divider} />
+
+        <ul className={css.privacy}>
+          <li className={css.privacyItem}>
+            © E-Pharmacy 2026. All Rights Reserved
+          </li>
+          <li className={css.privacyItem}>
+            <Link href={'/privacy'}>Privacy Policy</Link>
+          </li>
+          <li className={css.privacyItem}>
+            <Link href={'/terms'}>Terms & Conditions</Link>
+          </li>
+        </ul>
+      </Container>
+    </footer>
+  );
 }
--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,17 +1,40 @@
 import { ButtonLink } from '../ButtonLink/ButtonLink';
 import css from './NavBar.module.css';
+import clsx from 'clsx';
 
-export const NavBar = () => {
-    return (
-        <nav>
-            <ul className={css.navList}>
-                <li className={css.navItemWrapper}>
-                    <ButtonLink href='/'>Home</ButtonLink></li>
-                <li className={css.navItemWrapper}>
-                    <ButtonLink href='/medicine-store'>Medicine Store</ButtonLink></li>
-                <li className={css.navItemWrapper}>
-                     <ButtonLink href='/medicine'>Medicine</ButtonLink></li>
-            </ul>
-        </nav>
-    )
+interface NavBarProps {
+  variant: 'header' | 'footer';
 }
+
+export const NavBar = ({ variant }: NavBarProps) => {
+  return (
+    <nav>
+      <ul className={clsx(css.navList, variant === 'footer' && css.footerNav)}>
+        <li className={css.navItemWrapper}>
+          <ButtonLink
+            href="/"
+            className={clsx(variant === 'footer' ? css.footerButton : '')}
+          >
+            Home
+          </ButtonLink>
+        </li>
+        <li className={css.navItemWrapper}>
+          <ButtonLink
+            href="/medicine-store"
+            className={clsx(variant === 'footer' ? css.footerButton : '')}
+          >
+            Medicine Store
+          </ButtonLink>
+        </li>
+        <li className={css.navItemWrapper}>
+          <ButtonLink
+            href="/medicine"
+            className={clsx(variant === 'footer' ? css.footerButton : '')}
+          >
+            Medicine
+          </ButtonLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
