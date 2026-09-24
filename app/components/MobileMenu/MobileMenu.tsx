5d56fa9a71437ec02b6fb95687f9285ad5f45fd5
MarinaHurricane <maryishch88@gmail.com>
2026-09-25 00:34:33 +0100

    mobile menu link fix

--- app/components/MobileMenu/MobileMenu.tsx
+++ app/components/MobileMenu/MobileMenu.tsx
@@ -39,7 +39,7 @@ export const MobileMenu = ({
               </ButtonLink>
             </li>
             <li>
-              <ButtonLink href={'/location'} onClick={onClose}>
+              <ButtonLink href={'/locations'} onClick={onClose}>
                 Find a store
               </ButtonLink>
             </li>
